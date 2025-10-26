83844a4b7171fe7954878683a4be301ad6ee63c2
Jyoti <jyoti.tandon88@gmail.com>
2025-10-26 17:17:47 +0530

    Intro to Agentic AI

---
+++ AgenticAI/main_5_using_pre_built_agents.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+from langgraph.prebuilt import create_react_agent
+
+
+agent = create_react_agent(
+   model="groq:llama-3.3-70b-versatile", 
+   tools=[], 
+   prompt="You are a helpful assistant" 
+)
+
+
+# Run the agents
+response = agent.invoke(
+   {"messages": [{"role": "user", "content": "what are large language models"}]}
+)
+print(response)
+
+
+
+try:
+
+   img = agent.get_graph().draw_mermaid_png()
+   with open("graph2.png", "wb") as f:
+       f.write(img)
+except Exception:
+   pass
